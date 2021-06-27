39f6f4d42fef06596be81538a37f80643a464668
197PM21985 - HUỲNH PHÚC THIỆN - K25T-PM1 <thien.197pm21985@vanlanguni.vn>
2021-06-27 19:02:35 +0700

    Add project files.

---
+++ Quanlinhahangggg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quanlinhahangggg.Startup))]
+namespace Quanlinhahangggg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
